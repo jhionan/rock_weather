f51f54ab96d844310e094908d59b982cdb19fdc8
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-09 17:12:13 -0300

    feat:(structure) create app basic founder structure

---
+++ lib/core/navigation/navigation_config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rock_wheter/features/home/view/home.dart';
+import 'package:rock_wheter/features/weather_detail/view/weather_detail.dart';
+
+class AppRoutes {
+  static const String home = '/';
+  static const String detail = '/detail';
+  static Route<Object> onGenerateRoute(RouteSettings settings) {
+    Widget page;
+    switch (settings.name) {
+      case home:
+        page = BlocProvider(
+          create: (context) => null,
+          child: Home(),
+        );
+        break;
+      case detail:
+        page = BlocProvider(
+          create: (context) => null,
+          child: WeatherDetail(),
+        );
+        break;
+      default:
+        page = BlocProvider(
+          create: (context) => null,
+          child: Home(),
+        );
+        break;
+    }
+    return MaterialPageRoute(builder: (context) => page);
+  }
+}
---
+++ lib/features/weather_detail/view/weather_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
