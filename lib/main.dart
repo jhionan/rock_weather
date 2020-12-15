632138a240288be4f860b4713e399af0db1a4637
Jhionan Rian Lara dos Santos <jhionan@gmail.com>
2020-12-15 18:53:21 -0300

    lint: add project dependences and rename wrong package name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rock_weather/core/navigation/navigation_config.dart';
-import 'package:rock_weather/features/home/view/home.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        home: Home(),
         onGenerateRoute: AppRoutes.onGenerateRoute,
       );
 }
